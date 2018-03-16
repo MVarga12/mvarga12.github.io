ad57bfe25d9597a0a789ef4a4c4dc4490ee614f7
Matthew Varga <mvarga@JanusMac.local>
2018-03-16 14:11:17 -0400

    updated library and added shell script to quickly prepare it from JabRef export

---
+++ prep.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+####
+# simple code to take an html library created with JabRef and 
+# prepare it for use with jekyll, with inserted url for paper library
+# found in my google drive
+# mjv 3/16/18
+####
+
+printf "Preparing library %s" $1
+
+# remove the doctype line from the html library
+tail -n +2 $1 > tmp
+
+# create new header in the jekyll style
+printf "%s\nlayout:none\nurl: /library.html\n%s\n" '---' '---' > header
+
+# reinstert doctype line and url for the paper library
+printf "<!DOCTYPE HTML>\n" >> header
+printf "Click <a href=\"%s\">here</a> to go to documents." \
+    'https://drive.google.com/drive/folders/0B04bPEjpYXgPOENycDlySnQ2Y2M?usp=sharing'\
+    >> header
+
+# concatenate with the library and clean up
+cat header tmp > library.html
+rm tmp $1 header  
